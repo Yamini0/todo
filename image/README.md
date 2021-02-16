5953917f2fee2495b356fdb735fdab46410eda0c
Yamini0 <yaminis341@gmail.com>
2021-02-16 18:20:21 +0530

    To-Do_Application

---
+++ image/README.md
@@ -0,0 +1,5 @@
+# To-Do Application
+This todo application is build using JavaScript, HTML, CSS.
+
+UI interface:
+![](image/todo-png.png)
--- index.js
+++ index.js
@@ -1,82 +1,95 @@
-//selectors
-const todoInput = document.querySelector('.todo-input');
-const todoButton = document.querySelector('.todo-button');
-const todoList = document.querySelector('.todo-list');
-
-//eventslisterner
-todoButton.addEventListener('click',addTodo);
-todoList.addEventListener('click',deleteCheck);
-todoList.addEventListener('click',edit);
-
-//functions
-function addTodo(event){
-    event.preventDefault(); //stops the browser to get reload when click on button
-    //todo div
-    const todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo"); //class name of todoDiv is todo
-    //create li
-    const newTodo = document.createElement('li');
-    //adding element in tododiv(task)
-    newTodo.disable = true;
-    newTodo.innerHTML = todoInput.value;
-    newTodo.classList.add('todo-item');
-    todoDiv.appendChild(newTodo); //append the newtodo to tododiv
-
-    //completed button
-    const completedButton = document.createElement('button');
-    completedButton.innerHTML = "<i class='fa fa-check' aria-hidden='true'></i>";
-    completedButton.classList.add("complete-btn");
-    todoDiv.appendChild(completedButton);
-
-    //trash button
-    const deleteButton = document.createElement('button');
-    trashButton.innerHTML = "<i class='fa fa-trash' aria-hidden='true'></i>";
-    trashButton.classList.add("delete-btn");
-    todoDiv.appendChild(deleteButton);
-
-    //edit btn
-    const editbutton = document.createElement('button');
-    editbutton.innerHTML = "edit";
-    editbutton.classList.add('edit-btn');
-    todoDiv.appendChild(editbutton);
+/* Select all the necessary Elements  */
+var input = document.querySelector('.todo_input');
+var MainTodoContainer = document.getElementById('todos')
+var addingButton = document.querySelector('.add-item');
+var deleteAllBtn = document.querySelector('.deleteBtn');
+var completedButton = document.querySelector('.completed');
+var removeButton = document.querySelector('.trash');
 
 
-    //append all the todotask(todoDiv) to todolist
-    todoList.appendChild(todoDiv);
+//eventlisterners
+addingButton.addEventListener('click', addTodo);
 
-    //clear toinput value(user input wale box ka after adding delete from the inputbox)
-    todoInput.value = "";
-}
-
-
-function deleteCheck(event){
-    const item = event.target;
-    //delete todo
-    if(item.classList[0] === "delete-btn"){
-        const todo = item.parentElement;
-        todo.remove();
-    }
-
-    //check mark
-    if(item.classList[0] === "complete-btn"){
-        const todo =item.parentElement;
-        todo.classList.toggle('completed');
-    }
-
-    /*if(item.classList[0] === "edit-btn"){
-        const todo =item.parentElement;
-        item.parentElement.disabled = !item.parentElement.disabled;
-    }*/
-
+function addTodo(e){
+    //stops for reloading
+    e.preventDefault();
     
+    //Create all the elements
+    if(input.value.trim()){
+        // UL Tag 
+        var ulTag = document.createElement('ul');
+        ulTag.classList.add('todo-list-container');
+        // Todo list div 
+        var todoList = document.createElement('div');
+        todoList.classList.add('todo-list');
+        //LI Tag
+        var liTag = document.createElement('li');
+        liTag.innerText = input.value;
+        liTag.classList.add('todo-item');
+        // Button Div 
+        var buttonDiv = document.createElement('div');
+        buttonDiv.classList.add('button');
+        //completed button element1
+        var completeButton = document.createElement('button');
+        completeButton.classList.add('completed');
+        completeButton.innerHTML = '<i class="fas fa-check"></i>';
+        // Edit Button
+        var editBtn = document.createElement('button');
+        editBtn.innerHTML = '<i class="far fa-edit"></i>';
+        editBtn.classList.add('editBtn');
+        
+        //trash button element2
+        var trashButton = document.createElement('button');
+        trashButton.classList.add('trash');
+        trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    
+        //Appending Elements into each other 
+        ulTag.appendChild(todoList);
+        todoList.appendChild(liTag);
+        todoList.appendChild(buttonDiv);
+        buttonDiv.appendChild(completeButton);
+        buttonDiv.appendChild(editBtn);
+        buttonDiv.appendChild(trashButton);
+    
+        //if input is empty then don't display empty list in DOM
+        MainTodoContainer.appendChild(ulTag);
 
-}
-
-function edit(e){
-    const item = e.target; //return the clicked item inside the list
-        if(item.classList[0] === "edit-btn"){
-            const todo =item.parentElement;
-            item.parentElement.disable = !item.parentElement.disabled;
+        editBtn.onclick = function(){
+            editWorking(liTag);
         }
 
+        //when the add button click clear the input value 
+        input.value = '';
+
+
+        //complete and trash button working
+        todoList.addEventListener('click', function(e){
+            var items = e.target;
+            if(items.classList[0] === 'completed'){
+                var todo = items.parentElement;
+                var todo2 = todo.parentElement;
+                todo2.classList.toggle('line_through')
+            }
+            if(items.classList[0] === 'trash'){
+                var todo = items.parentElement;
+                var todo2 = todo.parentElement;
+                todo2.remove();
+            }
+        });
+    }else if(input.value === ''){
+        alert('Please fill the input field!')
     }
+}
+
+
+function editWorking(e){
+    var editValue = prompt('Edit the select item:', e.firstChild.nodeValue);
+    e.firstChild.nodeValue = editValue;
+}
+function deleteAllElements(){
+    var gettingUlTag = document.querySelectorAll('.todo-list-container');
+    for(var i = 0; i < gettingUlTag.length; i++){
+        gettingUlTag[i].remove();
+    }
+    input.value = '';
+}
